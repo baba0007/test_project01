30c95dde78e298209549ea682228d918f226accf
baba0007 <baba005@hotmail.com>
2022-02-07 18:41:53 +0100

    Delete networkin.py

--- networkin.py
+++
@@ -1,9 +0,0 @@
-import ipaddress
-net = ipaddress.IPv4Network(input('Enter Network address: '))
-count = 0
-for addr in net:
-    print(addr)
-    count += 1
-print(f'Number of ip address is : {count}')
-ip = ipaddress.ip_address(input('Enter ip address: '))
-print(f'{ip} is in {net} : {ip in net}')
