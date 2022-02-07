8d3369d1607e209bf1034605b257d3d9accbf986
baba0007 <baba005@hotmail.com>
2022-02-07 18:32:27 +0100

    Delete package2 directory

--- package2/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-print('#' * 30)
-print('Welcome to package2 Patterns...')
-print('Just call square, right_triangle, pyramid, diamond functions to print shapes...')
-print('#' * 30)
