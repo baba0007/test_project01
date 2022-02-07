c9f5164623e031eae990981c878a66c0a8c93b11
baba0007 <baba005@hotmail.com>
2022-02-07 18:44:23 +0100

    Delete sysinfo.py

--- sysinfo.py
+++
@@ -1,8 +0,0 @@
-import platform
-uname = platform.uname()
-print(f'System name :{uname.system}')
-print(f'Node name :{uname.node}')
-print(f'Release :{uname.release}')
-print(f'Version :{uname.version}')
-print(f'Machine name :{uname.machine}')
-print(f'Processor :{uname.processor}')
