e15e2292f302a04f5c1263c21e3aeb31d2adbec7
baba0007 <baba005@hotmail.com>
2022-02-07 18:08:01 +0100

    Delete browser directory

--- browser/flutter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#kill $(ps aux | grep "Safari")
-kill ps aux | grep "Chrome"
-python3 /Users/babadialo/Documents/python/wing/browser/browser-script.py /Users/babadialo/Documents/python/wing/browser/websites.txt chrome
-exit 0
-
