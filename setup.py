53431bf5a966fa88250e241eb10dc63137247bc3
baba0007 <baba005@hotmail.com>
2022-02-07 18:44:01 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-# setup(name='package2', version='0.1', packages=[
-#      'first_package2', 'second_package2', 'third_package2', 'etc...'])
-
-# or
-
-setup(name='package2', version='0.1', packages=find_packages())
