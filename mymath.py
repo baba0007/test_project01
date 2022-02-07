2d0effb0a6c180cfeaae89c2b99af5f7e4ef04da
baba0007 <baba005@hotmail.com>
2022-02-07 18:41:31 +0100

    Delete mymath.py

--- mymath.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    return a + b
-
-
-print(__name__)
-if __name__ == '__main__': # is used when we import this file to another file it wont let this result of this file to be returned in the other file
-    print(add(5, 15))
